5145984c1a70f0649536c2df124e0c8fd5663abb
Frankie Robertson <frankie@robertson.name>
2015-03-15 13:40:25 +0200

    Remove debugging trace

--- fetch_text.js
+++ fetch_text.js
@@ -21,7 +21,6 @@ var fetch=function(page, lang_or_wikiid, cb){
   var url;
   if (lang_or_wikiid.length !== 2) {
     init_site_map();
-    console.log(Object.keys(site_map), lang_or_wikiid);
     url=site_map[lang_or_wikiid]+'/w/index.php?action=raw&title='+page;
   } else {
     url='http://'+lang_or_wikiid+'.wikipedia.org/w/index.php?action=raw&title='+page;
