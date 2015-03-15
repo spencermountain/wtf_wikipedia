dc4fbf286d4c95be99070ec78ae2d93b0c6d39d2
Spencer Kelly <spencermountain@gmail.com>
2015-03-15 12:48:24 -0400

    use static sitemap to avoid mw cruft

--- fetch_text.js
+++ fetch_text.js
@@ -1,26 +1,10 @@
-var request=require("request")
-
-var SITE_MATRIX_URL = "https://en.wikipedia.org/w/api.php?action=sitematrix&format=json";
-var site_map = {};
-
-function init_site_map() {
-  if (Object.keys(site_map).length !== 0) {
-    return;
-  }
-  var sitematrix_raw=require("./sitematrix");
-  var sitematrix = sitematrix_raw.sitematrix;
-  Object.keys(sitematrix).forEach(function(key){
-    sitematrix[key].site.forEach(function(site){
-      site_map[site.dbname] = site.url;
-    });
-  });
-}
+var request= require("request")
+var site_map= require("./site_map")
 
 var fetch=function(page, lang_or_wikiid, cb){
   lang_or_wikiid = lang_or_wikiid || 'en';
   var url;
-  if (lang_or_wikiid.length !== 2) {
-    init_site_map();
+  if (site_map[lang_or_wikiid]) {
     url=site_map[lang_or_wikiid]+'/w/index.php?action=raw&title='+page;
   } else {
     url='http://'+lang_or_wikiid+'.wikipedia.org/w/index.php?action=raw&title='+page;
@@ -38,6 +22,7 @@ var fetch=function(page, lang_or_wikiid, cb){
 if(typeof module !== 'undefined' && module.exports) {
   module.exports = fetch;
 }
-// fetch("Radiohead", 'en', function(r){
+
+// fetch("Radiohead", 'en', function(r){ // 'afwiki'
 //   console.log(JSON.stringify(r, null, 2));
 // })
