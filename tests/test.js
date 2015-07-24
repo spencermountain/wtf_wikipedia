249aabc927524e5f08994af3d24ee3dac5df8e75
nathan spindel <nathans@gmail.com>
2015-07-24 13:00:16 -0700

    Add infobox_template parsing

--- tests/test.js
+++ tests/test.js
@@ -13,6 +13,7 @@ Tests['royal_cinema'] = function(test) {
    test.ok( data.infobox.opened.text, "INFOBOX");
    test.ok( data.text.Intro.length, 5);
    test.ok( data.categories.length, 4);
+   test.equal(data.infobox_template, 'venue')
    test.done();
 };
 
@@ -21,12 +22,14 @@ Tests['toronto_star'] = function(test) {
    test.ok( data.infobox.publisher.text, 'John D. Cruickshank');
    test.ok( data.text.History.length, 7);
    test.ok( data.categories.length, 6);
+   test.equal(data.infobox_template, 'newspaper')
    test.done();
 };
 
 Tests['jodie_emery'] = function(test) {
   var data=parser(fetch("jodie_emery"))
   test.ok(data.infobox.nationality.text, 'Canadian')
+  test.equal(data.infobox_template, 'person')
   test.ok(data.text.Intro.length>=1)
   test.ok(data.text['Political career'].length>=5)
   test.ok(data.categories.length, 8)
@@ -37,5 +40,6 @@ Tests['jodie_emery'] = function(test) {
 Tests['redirect'] = function(test) {
   var data=parser(fetch("redirect"))
   test.ok(data.redirect, 'Toronto')
+  test.ok(data.infobox_template == null)
   test.done();
 };
