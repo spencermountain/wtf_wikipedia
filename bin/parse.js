e7fd1e4e1cb4b241a2d04276236448e3919bbc62
spencermountain <spencermountain@gmail.com>
2017-03-24 15:11:42 -0400

    keep src directory after-all for bin files

--- bin/parse.js
+++ bin/parse.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
 
 'use strict';
-var wtf_wikipedia = require('../src/index');
-var fetch = require('../src/lib/fetch_text');
+var wtf = require('../src/index');
 
-var title = process.argv.slice(2, process.argv.length).join(" ");
-if(!title) {
+var title = process.argv.slice(2, process.argv.length).join(' ');
+if (!title) {
   throw new Error('Usage: wikipedia Toronto Blue Jays');
 }
-title = title.charAt(0).toUpperCase() + title.slice(1)
-  //fetch this topic's wikipedia page
-fetch(title, 'en', function (script) {
-  var data = wtf_wikipedia.parse(script)
+title = title.charAt(0).toUpperCase() + title.slice(1);
+//fetch this topic's wikipedia page
+wtf.from_api(title, 'en', function (script) {
+  var data = wtf.parse(script);
   console.log(JSON.stringify(data, null, 2));
-})
+});
