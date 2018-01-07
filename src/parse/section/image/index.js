d91f566cc15a479328f9ba64d80740dfa63dc724
ephdot <1666594+ephdot@users.noreply.github.com>
2018-01-06 19:27:47 -0500

    If namespace option is set, don't drop the text...
    ...if it matches.

--- src/parse/section/image/index.js
+++ src/parse/section/image/index.js
@@ -1,10 +1,9 @@
 const i18n = require('../../../data/i18n');
-const languages = require('../../../data/languages');
 const find_recursive = require('../../../lib/recursive_match');
 const parse_image = require('./image');
 const fileRegex = new RegExp('(' + i18n.images.concat(i18n.files).join('|') + '):.*?[\\|\\]]', 'i');
 
-const parseImages = function(r, wiki) {
+const parseImages = function(r, wiki, options) {
   //second, remove [[file:...[[]] ]] recursions
   let matches = find_recursive('[', ']', wiki);
   matches.forEach(function(s) {
@@ -15,15 +14,12 @@ const parseImages = function(r, wiki) {
     }
   });
 
-  var namespaceList = r.page_identifier != undefined ? r.page_identifier.match(/^(.*?):.*$/) : [];
-  var namespace = namespaceList.length > 1 ? namespaceList[1].toLowerCase() : "";
-
   //third, wiktionary-style interlanguage links
   matches.forEach(function(s) {
     if (s.match(/\[\[([a-z]+):(.*?)\]\]/i) !== null) {
       let site = (s.match(/\[\[([a-z]+):/i) || [])[1] || '';
       site = site.toLowerCase();
-      if (site && i18n.dictionary[site] === undefined && site !== namespace) {
+      if (site && i18n.dictionary[site] === undefined && !(options.namespace !== undefined && options.namespace === site) ) {
         r.interwiki = r.interwiki || {};
         r.interwiki[site] = (s.match(/\[\[([a-z]+):(.*?)\]\]/i) || [])[2];
         wiki = wiki.replace(s, '');
