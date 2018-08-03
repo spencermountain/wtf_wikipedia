c5a1875c1b1cf460e9ed5e7604f981d21d9ca893
spencer kelly <spencermountain@gmail.com>
2018-08-03 15:01:56 -0400

    gallery parsing tests for #138

--- tests/images.test.js
+++ tests/images.test.js
@@ -1,27 +1,9 @@
-const wtf = require('./src/index');
-// const readFile = require('./tests/lib/_cachedPage');
-// const wtf = require('./builds/wtf_wikipedia');
-// const wtf = require('./build');
+'use strict';
+var test = require('tape');
+var wtf = require('./lib');
 
-//325480
-wtf.fetch('Anders_Zorn', 'en', function(err, doc) {
-  console.log(doc.sections());
-});
-
-
-
-
-// let str = `
-// {{Infobox scientist
-// | name        = Albert Einstein
-// | image       = Einstein 1921 by F Schmutzer - restoration.jpg
-// | spouse      = {{nowrap| {{marriage|[[Elsa Löwenthal]]<br>|1919|1936|end=died}} }}
-// | residence   = Germany, Italy, Switzerland, Austria (present-day Czech Republic), Belgium, United States
-// | signature = Albert Einstein signature 1934.svg
-// }}
-// `;
-// console.log(wtf(str).infoboxes(0).json());
-let str = `
+test('gallery', t => {
+  var str = `
 hey cool!
 ==Paintings==
 While his early works were often brilliant, luminous [[watercolor]]s, by 1887 he had switched firmly to oils. Zorn was a prolific artist. He became an international success as one of the most acclaimed portrait painters of his era. His sitters included three American Presidents, nobility, the Swedish king and queen and numerous members of high society. Zorn also painted portraits of family members, friends, and self-portraits. Zorn is also famous for his nude paintings.<ref name="safran-arts.com"/> His fondness of painting full-figured women gave rise to the terms Zorn's ''kulla'' or ''dalakulla'', an unmarried woman or girl from [[Dalecarlia]], as the women were called in the local [[dialect]] of the region Zorn lived.
@@ -35,6 +17,13 @@ File:Anders Zorn - Ateljéidyll.jpg|G''Studio Idyll'', 1918
 </gallery>
 
 The paintings have the freedom and energy of  sketches, using warm and cool light and shade areas<ref name="safran-arts.com"/> with contrasting areas of warm and cool tones, and an understanding of colour contrasts and reflected lights. Zorn's accomplished use of the brush allows the forms and the texture of the painted subject to reflect and transmit light. In addition to portraits and nudes, Zorn excelled in realistic depictions of water, as well as scenes depicting rustic life and customs.
-`;
-// console.log(wtf(str).sections());
-// console.log(wtf(str).images());
+  `;
+  var doc = wtf(str);
+  t.deepEqual(doc.sections('paintings').templates('gallery').length, 1, 'section-has-gallery');
+  var templ = doc.templates(0);
+  t.deepEqual(templ.template, 'gallery', 'document-has-template');
+  t.deepEqual(templ.images.length, 5, '5 images');
+  t.deepEqual(templ.images[0].caption.links[0].page, 'Freyja', 'image has caption');
+  t.deepEqual(doc.images().length, 5, 'images() finds gallery');
+  t.end();
+});
