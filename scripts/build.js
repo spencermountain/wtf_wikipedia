53881df3348f8299213ce21700b0ade3b68f31a4
spencer kelly <spencermountain@gmail.com>
2018-08-08 16:33:00 -0400

    do nieberts idea for fixing #101

--- scripts/build.js
+++ scripts/build.js
@@ -1,27 +1,19 @@
 var exec = require('shelljs').exec;
 var echo = require('shelljs').echo;
 var fs = require('fs');
-//use paths, so libs don't need a -g
 var browserify = './node_modules/.bin/browserify';
 var derequire = './node_modules/.bin/derequire';
 var uglify = './node_modules/.bin/uglifyjs';
 
+// ok somehow,
+// for deploys, we want the 'browser' field
+// but that messes with browserify...
+// so temporarily remove it during builds.
+// ¯\_(ツ)_/¯
 var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-
-if (pkg.browser !== undefined) {
-  console.log(`
-
-----
-  Yo, please remove the "browser" property from package.json before building
-
-  sorry, somehow this is necessary to build-up the deps properly.
-   - I don't like computers either
-----
-
--stopping build.-
-  `);
-  process.exit(1);
-}
+exec('mv ./package.json ./package.json.backup');
+delete pkg.browser;
+fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2));
 
 //final build locations
 var banner = '/* wtf_wikipedia v' + pkg.version + '\n   github.com/spencermountain/wtf_wikipedia\n   MIT\n*/\n';
@@ -36,17 +28,19 @@ echo(banner).to(uncompressed);
 echo(banner).to(compressed);
 
 //browserify + derequire
-//var cmd = browserify + ' ./src/index.js --standalone wtf';
 var cmd = browserify + ' ./src/index.js --standalone wtf';
 cmd += ' -t [ babelify --presets [ env ] ]';
 cmd += ' | ' + derequire;
 cmd += ' >> ' + uncompressed;
-// console.log(cmd);
 exec(cmd);
 
 //uglify
 cmd = uglify + ' ' + uncompressed + ' --mangle --compress ';
 cmd += ' >> ' + compressed;
-exec(cmd); // --source-map ' + compressed + '.map'
+exec(cmd);
 
+//log the size of our builds
 require('./filesize');
+
+//.. then we replace original package.json file
+exec('mv ./package.json.backup ./package.json ');
