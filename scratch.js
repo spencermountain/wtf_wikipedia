13851ed760685ffe8f8c68ecd0f2edd2a617af15
spencer kelly <spencermountain@gmail.com>
2018-08-23 15:05:24 -0400

    get tests passing again

--- scratch.js
+++ scratch.js
@@ -3,15 +3,13 @@ const wtf = require('./src/index');
 // const wtf = require('./builds/wtf_wikipedia');
 // const wtf = require('./build');
 
-(async () => {
-  const document = await wtf.fetch('Formel 1', 'de');
+// (async () => {
+//   const document = await wtf.fetch('Formel 1', 'de');
+//
+//   console.log(document.section('Länderbezug').sentences(4).plaintext());
+// })();
 
-  console.log(document.section('Länderbezug').sentences(4).plaintext());
-})();
-
-// let str = `hello
-// [[Datei:Formula 1 all over the world-2018.svg|mini|hochkant=2|center|[[Liste der Formel-1-Rennstrecken|Länder, die einen Grand Prix austrugen]]
-// ]] world
-//  `;
+let str = `[[File:Tony Danza]]
+ `;
 // console.log(wtf(str).text());
-// console.log(wtf(str).images());
+console.log(wtf(str).images());
