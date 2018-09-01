0de865f992b106e388e0e75832b93dd6d1f8f2cd
spencer kelly <spencermountain@gmail.com>
2018-09-01 10:30:00 -0400

    fix for #153

--- scratch.js
+++ scratch.js
@@ -7,16 +7,7 @@ const wtf = require('./src/index');
 // const doc = await wtf.fetch('Berlin', 'de');
 // console.log(doc.sentences(0).plaintext());
 // })();
-let str = `{|class="wikitable sortable"
-!Name and Surname!!Height
-|-
-|data-sort-value="Smith, John"|John Smith||1.85
-|-
-|data-sort-value="Ray, Ian"|Ian Ray||1.89
-|-
-|data-sort-value="Bianchi, Zachary"|Zachary Bianchi||1.72
-|-
-!Average:||1.82
-|}`;
-let doc = wtf(str);
-console.log(doc.tables(0).data[0]);
+
+var doc=wtf("[[Fileaway|Fileaway]]")
+console.log(doc.text())
+//should return "Fileaway"; actually returns "away"
