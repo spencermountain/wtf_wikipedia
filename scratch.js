0b508f53a7e752b65e1c7ba978772bf6511177ad
spencer kelly <spencermountain@gmail.com>
2018-09-12 08:35:03 -0400

    fix for #167

--- scratch.js
+++ scratch.js
@@ -3,15 +3,15 @@ const wtf = require('./src/index');
 // const wtf = require('./builds/wtf_wikipedia');
 // const wtf = require('./build');
 
-(async () => {
-  const doc = await wtf.fetch(`List of submarine sandwich restaurants`, 'en');
-  console.log(doc.images());
-// console.log(doc.json({
-//   images: true
-// }));
-})();
+// (async () => {
+//   const doc = await wtf.fetch(`List of submarine sandwich restaurants`, 'en');
+//   console.log(doc.images());
+// // console.log(doc.json({
+// //   images: true
+// // }));
+// })();
 
 
 
-// let str = `hello [[List of highest funded crowdfunding projects|highest funded crowdfunding projects]] world`;
-// console.log(wtf(str).links());
+let str = `hello [[List of highest funded crowdfunding projects|highest funded crowdfunding projects]] world`;
+console.log(wtf(str).links());
