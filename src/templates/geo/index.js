274e7f162a72f1ec7928e412be801149451995c4
wouter van der plas <wouterv.dplas@gmail.com>
2018-10-17 02:03:01 +0200

    add Coor template

--- src/templates/geo/index.js
+++ src/templates/geo/index.js
@@ -1,4 +1,5 @@
 const parseCoord = require('./coord');
+const parseCoor = require('./coor');
 const strip = require('../parsers/_strip');
 
 //
@@ -6,6 +7,27 @@ const geoTemplates = {
   coord: (tmpl) => {
     tmpl = strip(tmpl);
     return parseCoord(tmpl);
+  },
+  // these are from the nl wiki
+  'coor title dms': (tmpl) => {
+    tmpl = strip(tmpl);
+    return parseCoor(tmpl);
+  },
+  'coor title dec': (tmpl) => {
+    tmpl = strip(tmpl);
+    return parseCoor(tmpl);
+  },
+  'coor dms': (tmpl) => {
+    tmpl = strip(tmpl);
+    return parseCoor(tmpl);
+  },
+  'coor dm': (tmpl) => {
+    tmpl = strip(tmpl);
+    return parseCoor(tmpl);
+  },
+  'coor dec': (tmpl) => {
+    tmpl = strip(tmpl);
+    return parseCoor(tmpl);
   }
 };
 module.exports = geoTemplates;
