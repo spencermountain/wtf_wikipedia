fa18eb074cac520bc8e9dbd83d13102abae6ea6a
spencer kelly <spencermountain@gmail.com>
2018-11-26 13:26:51 -0500

    start category, template fetch, readme

--- scratch.js
+++ scratch.js
@@ -10,32 +10,43 @@ const wtf = require('./src/index');
 
 // let doc = readFile('jodie_emery');
 // console.log(doc.markdown());
-// let str = `{| class="wikitable" align="center"
-// | fun
-// | cool
-// |}
-// `;
-// str = `<chem>{C_mathit{x}H_mathit{y}} + mathit{z}O2 -> {mathit{x}CO2} +rac{mathit{y}}{2}H2O</chem> `;
-// str = `{{chem|link=oxygen|O|2}} `;
-// str = `{{coord|40.7127|N|74.0059|W|region:US-NY|format=dms|display=inline,title}}`;
-// str = `  {{tryit
-//   | name = New York City
-//   | official_name                   = City of New York
-//   | settlement_type                 = [[City]]
-//   | named_for                       = [[James II of England|James, Duke of York]]
-//   | coordinates                     =
-//   }}`;
-// str = `hello {{citation |url=cool.com/?fun=yes/   }}{{CITE book |title=the killer and the cartoons }}`;
-// str = `{{cite book |editor-last=Heilbron |editor-first=John L. |url=https://books.google.com/books?id=abqjP-_KfzkC&pg=PA233 |isbn=978-0-19-974376-6 |page=233}}`;
-// str = `hello {{citation |url=cool.com/?fun=yes/   }}{{CITE book |title=the killer and the cartoons }}`;
-// str = `hello {{main|lksdf|asdf }}`;
-// str = `{{term|1=A-show}}`;
 
-// let doc = wtf(str);
-// console.log(doc.text());
-// console.log(doc.templates());
+let str = `
+==Player statistics==
+{{NBA roster statistics legend}}
 
-wtf.random('de').then(doc => console.log(doc.title()));
+;Cleveland Cavaliers
+{{NBA roster statistics start|team=Cleveland Cavaliers}}
+|-
+| style="text-align:left;"| {{sortname|Matthew|Dellavedova}} || 6 || 0 || 7.6 || .263 || .167 || .833 || 0.5 || 1.0 || 0.0 || 0.0 || 2.7
+|-
+| style="text-align:left;"| {{sortname|Channing|Frye}} || 4 || 0 || 8.3 || .000 || .000 || '''1.000''' || 0.8 || 0.0 || 0.0 || 0.5 || 0.5
+|-
+| style="text-align:left;"| {{sortname|Kyrie|Irving}} || 7 || 7 || 39.0 || .468 || '''.405''' || .939 || 3.9 || 3.9 || 2.1 || 0.7 || 27.1
+|-! style="background:#FDE910;"
+| style="text-align:left;"| {{sortname|LeBron|James}} || 7 || 7 || '''41.7''' || .494 || .371 || .721 || '''11.3''' || '''8.9''' || '''2.6''' || '''2.3''' || '''29.7'''
+|-
+| style="text-align:left;"| {{sortname|Richard|Jefferson}} || 7 || 2 || 24.0 || .516 || .167 || .636 || 5.3 || 0.4 || 1.3 || 0.1 || 5.7
+|-
+| style="text-align:left;"| {{sortname|Dahntay|Jones}} || 6 || 0 || 3.0 || .500 || .000 || .800 || 0.3 || 0.0 || 0.0 || 0.2 || 1.3
+|-
+| style="text-align:left;"| {{sortname|James|Jones|dab=basketball player}} || 5 || 0 || 4.0 || .000 || .000 || .250 || 0.4 || 0.4 || 0.0 || 0.0 || 0.2
+|-
+| style="text-align:left;"| {{sortname|Kevin|Love}} || 6 || 5 || 26.3 || .362 || .263 || .706 || 6.8 || 1.3 || 0.7 || 0.3 || 8.5
+|-
+| style="text-align:left;"| {{sortname|Jordan|McRae}} || 1 || 0 || 3.0 || '''1.000''' || .000 || .000 || 1.0 || 0.0 || 0.0 || 0.0 || 4.0
+|-
+| style="text-align:left;"| {{sortname|Timofey|Mozgov}} || 5 || 0 || 5.0 || .333 || .000 || .750 || 1.6 || 0.0 || 0.6 || 0.2 || 1.4
+|-
+| style="text-align:left;"| {{sortname|Iman|Shumpert}} || 7 || 0 || 18.3 || .304 || .267 || '''1.000''' || 1.6 || 0.1 || 0.1 || 0.3 || 3.0
+|-
+| style="text-align:left;"| {{sortname|J. R.|Smith}} || 7 || 7 || 37.3 || .400 || .356 || .667 || 2.7 || 1.6 || 1.4 || 0.3 || 10.6
+|-
+| style="text-align:left;"| {{sortname|Tristan|Thompson}} || 7 || 7 || 32.3 || .636 || .000 || .533 || 10.1 || 0.7 || 0.3 || 0.9 || 10.3
+|-
+| style="text-align:left;"| {{sortname|Mo|Williams}} || 6 || 0 || 4.8 || .333 || .200 || .000 || 0.5 || 0.2 || 0.5 || 0.0 || 1.5
+{{s-end}}`;
+let doc = wtf(str);
+console.log(doc.plaintext());
 
-// console.log(doc.citations());
-// console.log(doc.links(0));
+// wtf.random('de').then(doc => console.log(doc.title()));
