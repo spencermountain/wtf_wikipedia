285a15f4891408a471dff6069591e45cdfaf280c
spencer kelly <spencermountain@gmail.com>
2018-11-27 11:15:26 -0500

    support rowspan

--- src/02-section/table/parseTable.js
+++ src/02-section/table/parseTable.js
@@ -1,13 +1,15 @@
 const parseSentence = require('../../04-sentence/').oneSentence;
-const findRows = require('./findRows');
+const findRows = require('./_findRows');
+const handleSpans = require('./_spans');
 
 //additional table-cruft to remove before parseLine method
 const cleanText = function(str) {
+  // str = parseSentence(str).text();
   //anything before a single-pipe is styling, so remove it
   if (str.match(/\|/)) {
     str = str.replace(/.+\| ?/, ''); //class="unsortable"|title
   }
-  str = str.replace(/style=".*?"/, '');
+  str = str.replace(/style=['"].*?["']/, '');
   //'!' is used as a highlighed-column
   str = str.replace(/^!/, '');
   return str;
@@ -28,22 +30,31 @@ const findHeaders = function(rows) {
   return headers;
 };
 
+// let cell = parseSentence(str);
+// cell.text(cleanText(cell.text()));
+
+//turn headers, array into an object
+const parseRow = function(arr, headers) {
+  let row = {};
+  arr.forEach((str, i) => {
+    let h = headers[i] || 'col' + (i + 1);
+    let s = parseSentence(str);
+    s.text(cleanText(s.text()));
+    row[h] = s;
+  });
+  return row;
+};
+
 //turn a {|...table string into an array of arrays
 const parseTable = function(wiki) {
   let lines = wiki.replace(/\r/g, '').split(/\n/);
   lines = lines.map(l => l.trim());
   let rows = findRows(lines);
   let headers = findHeaders(rows);
+  rows = handleSpans(rows);
   //index them by their header
   let table = rows.map(arr => {
-    let row = {};
-    arr.forEach((str, i) => {
-      let header = headers[i] || 'col' + (i + 1);
-      let cell = parseSentence(str);
-      cell.text(cleanText(cell.text()));
-      row[header] = cell;
-    });
-    return row;
+    return parseRow(arr, headers);
   });
   return table;
 };
