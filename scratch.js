6899065e6bb709aeeac822fd6695da8f2bac3363
spencer kelly <spencermountain@gmail.com>
2018-12-18 11:14:27 -0500

    fix for

--- scratch.js
+++ scratch.js
@@ -3,11 +3,11 @@ const wtf = require('./src/index');
 // const wtf = require('./builds/wtf_wikipedia');
 // const wtf = require('./build');
 
-// (async () => {
-//   var doc = await wtf.fetch('Buzz Aldrin');
-//   // var doc = await wtf.random();
-//   console.log(doc.infobox(0).json());
-// })();
+(async () => {
+  var doc = await wtf.fetch('HSBC');
+  // var doc = await wtf.random();
+  console.log(doc.json());
+})();
 
 // let doc = readFile('BBDO');
 // console.log(doc.infoboxes(0).data);
@@ -19,19 +19,19 @@ const wtf = require('./src/index');
 // });
 
 
-let str = `{{columns-list|colwidth=15em|
-*[[Doug Acomb]]
-*[[Doug Adam]]
-*[[Gary Aldcorn]]
-*[[Mike Amodeo]]
-*[[John Anderson (ice hockey)|John Anderson]]
-*'''[[George Armstrong (ice hockey)|George Armstrong]]'''
-*[[Tim Armstrong (ice hockey)|Tim Armstrong]]
-*[[Earl Balfour]]
-}}`;
-
-let doc = wtf(str);
-console.log(doc.text());
-console.log(wtf(str).templates(0));
+// let str = `{{columns-list|colwidth=15em|
+// *[[Doug Acomb]]
+// *[[Doug Adam]]
+// *[[Gary Aldcorn]]
+// *[[Mike Amodeo]]
+// *[[John Anderson (ice hockey)|John Anderson]]
+// *'''[[George Armstrong (ice hockey)|George Armstrong]]'''
+// *[[Tim Armstrong (ice hockey)|Tim Armstrong]]
+// *[[Earl Balfour]]
+// }}`;
+//
+// let doc = wtf(str);
+// console.log(doc.text());
+// console.log(wtf(str).templates(0));
 
 // Alan Bean marriage template
