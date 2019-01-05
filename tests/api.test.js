45c156ce8065f00ec4d3212ecfb3783e35b987bd
spencer kelly <spencermountain@gmail.com>
2019-01-05 15:38:47 -0500

    improved test coverage

--- tests/api.test.js
+++ tests/api.test.js
@@ -12,12 +12,15 @@ var readFile = function(file) {
 
 var pages = [
   'al_Haytham',
-  'Senate_of_Pakistan',
-  'Sara-C.-Bisel',
   'Mozilla-Firefox',
-  'Teymanak-e-Olya',
-  'The-Field-of-Waterloo',
-  'RNDIS',
+  'anarchism',
+  'toronto',
+  'toronto_star',
+  'royal_cinema',
+  'jodie_emery',
+  'Allen-R.-Morris',
+  'United-Kingdom',
+  'Irina-Saratovtseva'
 ];
 
 test('Document-methods-do-not-throw', t => {
@@ -50,6 +53,95 @@ test('Sentence-methods-do-not-throw', t => {
     var doc = wtf(readFile(page));
     var sen = doc.sentences(0);
     docs.Sentence.forEach((obj) => {
+      var desc = obj.name + ' - ' + page;
+      sen[obj.name]();
+      t.ok(true, desc);
+    });
+  });
+  t.end();
+});
+
+test('Reference-methods-do-not-throw', t => {
+  pages.forEach((page) => {
+    var doc = wtf(readFile(page));
+    var sen = doc.references(0);
+    docs.Reference.forEach((obj) => {
+      var desc = obj.name + ' - ' + page;
+      sen[obj.name]();
+      t.ok(true, desc);
+    });
+  });
+  t.end();
+});
+
+test('Image-methods-do-not-throw', t => {
+  pages.forEach((page) => {
+    var doc = wtf(readFile(page));
+    var sen = doc.images(0);
+    docs.Image.forEach((obj) => {
+      var desc = obj.name + ' - ' + page;
+      sen[obj.name]();
+      t.ok(true, desc);
+    });
+  });
+  t.end();
+});
+
+test('Infobox-methods-do-not-throw', t => {
+  var mypages = [
+    'al_Haytham',
+    'Mozilla-Firefox',
+    'toronto',
+    'toronto_star',
+    'royal_cinema',
+    'jodie_emery',
+    'Allen-R.-Morris',
+    'Irina-Saratovtseva'
+  ];
+  mypages.forEach((page) => {
+    var doc = wtf(readFile(page));
+    var sen = doc.infoboxes(0);
+    docs.Infobox.forEach((obj) => {
+      var desc = obj.name + ' - ' + page;
+      sen[obj.name]();
+      t.ok(true, desc);
+    });
+  });
+  t.end();
+});
+
+test('List-methods-do-not-throw', t => {
+  var mypages = [
+    'al_Haytham',
+    'Mozilla-Firefox',
+    'toronto',
+    'toronto_star',
+    'jodie_emery',
+    'Allen-R.-Morris'
+  ];
+  mypages.forEach((page) => {
+    var doc = wtf(readFile(page));
+    var sen = doc.lists(0);
+    docs.List.forEach((obj) => {
+      var desc = obj.name + ' - ' + page;
+      sen[obj.name]();
+      t.ok(true, desc);
+    });
+  });
+  t.end();
+});
+
+test('Table-methods-do-not-throw', t => {
+  var mypages = [
+    'Mozilla-Firefox',
+    'toronto',
+    'Allen-R.-Morris',
+    'bluejays'
+  ];
+  mypages.forEach((page) => {
+    var doc = wtf(readFile(page));
+    var sen = doc.tables(0);
+    docs.Table.forEach((obj) => {
       var desc = obj.name + ' - ' + page;
       sen[obj.name]();
       t.ok(true, desc);
