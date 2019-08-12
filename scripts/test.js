76a159235157bab7e55ddbbb6c3ae621e32d4691
spencer kelly <spencermountain@gmail.com>
2019-08-12 11:49:41 -0400

    7.7.0

--- scripts/test.js
+++
@@ -1,7 +0,0 @@
-var exec = require('shelljs').exec
-var tape = './node_modules/.bin/tape'
-var spec = './node_modules/.bin/tap-dancer --color'
-// var spec = '/Users/spencer/mountain/tap-dance/bin/cmd.js --color';
-
-//run tests server-side
-exec(tape + ' ./tests/*.test.js | ' + spec)
