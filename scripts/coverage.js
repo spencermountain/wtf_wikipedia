9a0e4a0aaaa0db46ef40fa9ed38a4b0bdfb19fdb
Wouter van der Plas <wouterv.dplas@gmail.com>
2020-10-03 12:17:26 +0200

    Add CodeCov and created a GitHub Action For code Coverage

--- scripts/coverage.js
+++
@@ -1,7 +0,0 @@
-const exec = require('shelljs').exec
-const codecov = '62d2cc94-1c1f-4e21-a9af-42cc0cf39c51' //i don't really care if you steal this.
-
-//run all the tests
-const cmd = `./node_modules/.bin/nyc --reporter=text-lcov ./node_modules/.bin/tape ./tests/**/*.test.js > coverage.lcov && ./node_modules/.bin/codecov -t ${codecov}`
-exec(cmd)
-console.log('\n 🏃 done!')
