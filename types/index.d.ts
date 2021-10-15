a27e62945dd6b15f2da413e91230f39a2949834b
Wouter van der Plas <wouterv.dplas@gmail.com>
2021-10-15 15:27:30 +0200

    updated typescript types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,408 +1,300 @@
-// Type definitions for wtf_wikipedia 7.3.0
-// Project: https://github.com/spencermountain/wtf_wikipedia#readme
-// Definitions by: Rob Rose <https://github.com/RobRoseKnows>
-//                 Mr. Xyfir <https://github.com/MrXyfir>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
-// export = wtf
-export as namespace wtf
-
-declare function wtf(wiki: string, options?: any): wtf.Document
-
-declare module wtf {
-  /** current version of the library */
-  export const version: string
-
-  /** fetch articles belonging to this wikipedia category */
-  export function category(cat: string, lang?: string, options?: object, cb?: any): Promise<object>
-
-  /** extend built-in functionality */
-  export function extend(fn: any): any
-  /** extend built-in functionality */
-  export function plugin(fn: any): any
-
-  /** grab wikipedia content from an API */
-  export function fetch(titleOrId: string | number | number[] | string[], options?: Record<string, any>, callback?: (error: unknown, result: null | Document | Document[]) => void): Promise<null | Document | Document[]>
-  
-  /** grab a random article from a wikimedia API */
-  export function random(lang?: string, options?: object, cb?: any): Promise<Document>
-
-  class Document {
-    private data: object
-
-    private options: object
-
-    /**get, set, or guess the title of the page */
-    title(str?: string): string
-    /**get/set the wikimedia id for the page */
-    pageID(str?: string | number): string
-    /**get/set the wikidata id for the page */
-    wikidata(str?: string | number): string
-    /**get/set the domain of the wiki */
-    domain(str?: string | number): string
-    /**get/set the wikimedia namespace for the page */
-    namespace(str?: string | number): string
-    /**get/set the language for the page */
-    language(str?: string): string
-    /** try to create the url of the page */
-    url(): string | null
-
-    /**if the page is just a redirect to another page */
-    isRedirect(): boolean
-
-    /**the page this redirects to */
-    redirectTo(): Document
-    /** Alias of redirectTo */
-    redirectsTo(): Document
-    /** Alias of redirectTo */
-    redirect(): Document
-    /** Alias of redirectTo */
-    redirects(): Document
-
-    /**is this a placeholder page to direct you to one-of-many possible pages */
-    isDisambiguation(): boolean
-
-    /** Alias of isDisambiguation */
-    isDisambig(): boolean
-
-    /** fetch a list of this page's categories */
-    categories(clue?: number): string[]
-    /** fetch the first or nth category */
-    category(clue?: number): string
-
-    /**return a list, or given-index of the Document's sections */
-    sections(clue?: number): Section[]
-    /** fetch the nth section */
-    sections(clue?: string): Section[]
-    /** fetch the first Section */
-    section(clue?: number | string): Section
-
-    /**return a list, or given-index of Paragraphs, in all sections */
-    paragraphs(clue?: number): Paragraph[]
-    /**grab the first paragraph */
-    paragraph(n?: number): Paragraph
-
-    /** list of all sentences in the document*/
-    sentences(clue?: number): Sentence[]
-    /** return the first sentence in the document*/
-    sentence(n?: number): Sentence
-
-    /** return all images in the document */
-    images(clue?: number): Image[]
-    /** return the first image in the document */
-    image(n?: number): Image
-
-    /**list of all links in the document */
-    links(clue?: string): Link[]
-    link(clue?: number | string): Link
-
-    /**any links to other language wikis */
-    interwiki(clue?: string): object[]
-    interwiki(clue?: number): object
-
-    /**sections in a page where each line begins with a bullet point */
-    lists(clue?: number): List[]
-    lists(n?: number): List
-
-    /**list of all structured tables in the document */
-    tables(clue?: number): Table[]
-    table(n?: number): Table
-
-    /**list any type of structured-data elements, typically wrapped in like {{this}} */
-    templates(clue?: string): Template[]
-    template(clue?: number): Template
-
-    /**list of 'citations' in the document */
-    references(clue?: string): Reference[]
-    reference(clue?: number): Reference
-
-    /** Alias of references */
-    citations(clue?: string): Reference[]
-    /** Alias of reference */
-    citation(clue?: number): Reference
-
-    /**geo-locations that appear on the page */
-    coordinates(clue?: string): object[]
-    coordinate(clue?: string|number): object
-
-    /**specific type of template, that appear on the top-right of the page */
-    infoboxes(clue?: number): Infobox[]
-    infobox(clue?: number): Infobox
-
-    /**plaintext, human-readable output for the page */
-    text(options?: object): string
-    /**a 'stringifyable' output of the page's main data */
-    json(options?: object): object
-    /**original markup text */
-    wikitext(): string
-    /** helper information for the document */
-    debug(): Document
-  }
-
-  class Section {
-    private doc: Document
-    private _title: string
-    private data: object
-    private depth: number
-
-    /**the name of the section, between ==these tags== */
-    title(): string
-    /**which number section is this, in the whole document. */
-    index(): null | number
-    /**how many steps deep into the table of contents it is */
-    indentation(): number
-
-    sentences(clue?: number): Sentence[]
-    sentence(n: number): Sentence
-
-    paragraphs(clue?: number): Paragraph[]
-    paragraph(n?: number): Paragraph
-
-    links(n?: string): object[]
-    link(n?: number): object
-
-    tables(clue?: number): Table[]
-    table(n?: number): Table
-
-    templates(clue?: string): Template[]
-    template(clue?: number): Template
-
-    infoboxes(clue?: number): Infobox[]
-    infoboxe(clue?: number): Infobox
-
-    coordinates(clue?: number): object[]
-    coordinate(clue?: number): object
-
-    lists(clue?: number): List[]
-    list(clue?: number): List
-
-    /**any links to other language wikis */
-    interwiki(clue?: number): object[]
-    interwikis(num: number): object
-
-    /**return a list of any images in this section */
-    images(clue?: number): Image[]
-    image(clue?: number): Image
-
-    references(clue?: string): Reference[]
-    reference(clue?: number|string): Reference
-    /** Alias of references() */
-    citations(clue?: string): Reference
-    /** Alias of reference() */
-    citation(clue?: number|string): Reference[]
-
-    /**remove the current section from the document */
-    remove(): Document
-
-    nextSibling(): Section | null
-
-    /** Alias of nextSibling() */
-    next(): Section | null
-
-    /**a section following this one, under the current parent: eg. 1920s → 1930s  */
-    lastSibling(): Section | null
-    /** Alias of lastSibling() */
-    last(): Section | null
-    /** Alias of lastSibling() */
-    previousSibling(): Section | null
-    /** Alias of lastSibling() */
-    previous(): Section | null
-    /**any sections more specific than this one: eg. History → [PreHistory, 1920s, 1930s] */
-    children(n?: string): Section[]
-    child(n: number): Section
-    /** Alias of children */
-    sections(n?: string): Section[]
-    /** Alias of children */
-    section(n?: number): Section
-    /**the section, broader than this one: eg. 1920s → History */
-    parent(): null | Section
-    /** readable plaintext */
-    text(options?: object): string
-    /** all parsed data */
-    json(options?: object): object
-    /**original markup text */
-    wikitext(): string
-  }
-
-  class Infobox {
-    private _type: string
-
-    type(): string
-    /** Alias of type() */
-    template(): string
-
-    links(n?: string): object[]
-
-    image(): Image | null
-
-    /** Alias of image() */
-    images(): Image | null//FIXME
-
-    get(key: string): object | null
-
-    keyValue(): object
-
-    /** Alias of keyValue() */
-    data(): object
-
-    /** readable plaintext */
-    text(): string
-    /** all parsed data */
-    json(options?: object): object
-    /** original wiki markup */
-    wikitext(): string
-  }
-
-  class Template {
-    private data: object
-    /** readable plaintext */
-    text(): string
-    /** all parsed data */
-    json(options?: object): object
-    /** original wiki markup */
-    wikitext(): string
-  }
-
-  class Table {
-    private data: object
-
-    links(n?: string): object[]
-
-    keyValue(options?: object): object
-
-    /** Alias of keyValue */
-    keyvalue(options?: object): object
-
-    // Alais of keyValue
-    keyval(options?: object): object
-    /** readable plaintext */
-    text(): string
-    /** all parsed data */
-    json(options?: object): object
-    /** original wiki markup */
-    wikitext(): string
-  }
-
-  class Reference {
-    private data: object
-
-    title(): string
-
-    links(n: number): object
-
-    links(n?: string): object[]
-     /** readable plaintext */
-    text(): string
-    /** all parsed data */
-    json(options?: object): object
-    /** original wiki markup */
-    wikitext(): string
-  }
-
-  class Paragraph {
-    private data: object
-
-    sentences(): Sentence[]
-
-    references(clue?: string|number): Reference[]
-    reference(): Reference
-
-    /** Alias of references */
-    citations(clue?: string): Reference []
-    /** Alias of references */
-    citation(clue?: string|number): Reference[]
-
-    lists(): List[]
-    lists(): List
-
-    images(): Image[]
-    images(clue?: number | string): Image
-
-
-    links(clue?: string|number): object
-    link(clue?: string|number): object
-
-    interwiki(clue?: number): object
-    interwiki(): object[]
-    /** readable plaintext */
-    text(options?: object): string
-    /** all parsed data */
-    json(options?: object): object
-    /** original wiki markup */
-    wikitext(): string
-  }
-
-  class Image {
-    private data: object
-
-    file(): string
-
-    alt(): string
-
-    caption(): string
-
-    links(): object[]
-
-    url(): string
-
-    /** Alias of url() */
-    src(): string
-
-    thumbnail(size?: number): string
-
-    /** Alias of thumbnail() */
-    thumb(size?: number): string
-
-    format(): string
-    /** readable plaintext */
-    text(): string
-    /** all parsed data */
-    json(options?: object): object
-    /** original wiki markup */
-    wikitext(): string
-  }
-
-  class List {
-    private data: object
-
-    lines(): object[]
-
-    links(clue?: number): object[]
-    // link(n?: string): object
-
-    interwiki(clue?: number): object[]
-
-    interwiki(): object[]
-    /** readable plaintext */
-    text(options?: object): string
-    /** all parsed data */
-    json(options?: object): object
-    /** original wiki markup */
-    wikitext(): string
-  }
-
-  class Sentence {
-    private data: object
-
-    links(clue?: string|number): object
-    link(clue?: string|number): object
-
-    interwiki(clue?: number|string): object
-    interwiki(): object[]
-
-    bolds(clue?: number|string): string[]
-    bold(clue?:number|string): string
-
-
-    italics(clue?: number|string): string[]
-    italic(clue?: number|string): string
-    /** readable plaintext */
-    text(str?: string): string
-    /** all parsed data */
-    json(options?: object): object
-    /** original wiki markup */
-    wikitext(): string
-  }
+declare class Document {
+  constructor(wiki?: string, options?: object)
+  _pageID: any
+  _namespace: any
+  _lang: any
+  _domain: any
+  _title: any
+  _type: string
+  _redirectTo: any
+  _wikidata: any
+  _wiki: string
+  _categories: string[]
+  _sections: any[]
+  _coordinates: any[]
+
+  categories(clue?: number): string[]
+  category(clue?: number): string | null
+  citations(clue?: number): Reference[]
+  coordinate(clue?: number): object | null
+  coordinates(clue?: number): object[]
+  debug(): Document
+  domain(str?: string): string | null
+  images(clue?: string | number): Image[]
+  infoboxes(clue?: number): Infobox[]
+  interwiki(clue?: number): string[]
+  isDisambig: () => boolean
+  isDisambiguation(): boolean
+  isRedirect(): boolean
+  json(options?: object): object
+  lang: (lang?: string) => string | null
+  language(lang?: string): string | null
+  links(clue?: number): string[]
+  lists(clue?: number): List[]
+  namespace(ns?: string): string | null
+  ns: (ns?: string) => string | null
+  pageID(id?: number): number | null
+  paragraph(clue?: string | number): Paragraph | null
+  paragraphs(clue?: string | number): Paragraph[]
+  plaintext: (options?: object) => string
+  redirect: () => null | object
+  redirects: () => null | object
+  redirectsTo: () => null | object
+  redirectTo(): null | object
+  references(clue?: number): Reference[]
+  section(clue?: string | number): Section | null
+  sections(clue?: string | number): Section[]
+  sentence(clue?: number): Sentence | null
+  sentences(clue?: string | number): Sentence[]
+  tables(clue?: number): List[]
+  templates(clue?: number): List[]
+  text(options?: object): string
+  title(str?: string): null | string
+  url(): string | null
+  wikidata(id?: string): string | null
+  wikitext(): string
 }
 
-export default wtf
+declare class Section {
+  private doc: Document
+  private _title: string
+  private data: object
+  private depth: number
+
+  children(clue?: string | number): Section | Section[] | null
+  citations: () => object | object[]
+  coordinates(): object | object[]
+  depth(): number
+  images(): Image | Image[]
+  indentation(): number
+  index(): number | null
+  infoboxes(clue?: string | number): object | object[]
+  interwiki(): object | object[]
+  json(options: object): object
+  last(): Section | null
+  lastSibling(): Section | null
+  links(clue?: string | number): object | object[]
+  lists(): object | object[]
+  next(): Section | null
+  nextSibling(): Section | null
+  paragraphs(): object | object[]
+  parent(): Section | null
+  previous(): Section | null
+  previousSibling(): Section | null
+  references(): object | object[]
+  remove(): null | Document
+  sections(clue?: string | number): Section | Section[] | null
+  sentences(): object | object[]
+  tables(): object | object[]
+  templates(clue?: string | number): object | object[]
+  text(options: object): string
+  title(): string
+  wikitext(): string
+}
+
+declare class Infobox {
+  constructor(obj: object, wiki: string)
+  _type: any
+  _domain: any
+  _wiki: string
+  _data: any
+
+  data: () => object
+  get(keys: string | string[]): Sentence | undefined | unknown
+  image(): Image | null
+  images: () => Image | null
+  json(options: object): object
+  keyValue(): object
+  links(clue?: string): Link[]
+  template: () => string
+  text(): string
+  type(): string
+  wikitext(): string
+}
+
+declare class Template {
+  constructor(data: object, text?: string, wiki?: string)
+  _data: any
+  _text: string
+  _wiki: string
+
+  json(): object
+  text(): string
+  wikitext(): string
+}
+
+declare class Table {
+  constructor(data: any[], wiki?: string)
+  _data: any[]
+  _wiki: string
+
+  get(keys?: string | string[]): object
+  json(options: object): object
+  keyval(options: object): object
+  keyValue(options: object): object
+  keyvalue(options: object): object
+  links(n?: string): Link[]
+  text(): string
+  wikitext(): string
+}
+
+declare class Reference {
+  constructor(data: object, wiki: string)
+  _data: any
+  _wiki: string
+
+  json(options?: object): object
+  links(n?: string | number): Link[]
+  text(): string
+  title(): string
+  wikitext(): string
+}
+
+declare class Paragraph {
+  constructor(data: {
+    wiki?: string | undefined
+    lists?: List[] | undefined
+    sentences?: Sentence[] | undefined
+    images?: Image[] | undefined
+    references?: Reference[] | undefined
+  })
+  _sentences: Sentence[]
+  _references: Reference[]
+  _lists: List[]
+  _images: Image[]
+  _wiki: string
+
+  images(): Image[]
+  interwiki(): Link[]
+  json(options: object): object
+  links(clue: string): Link[]
+  lists(): List[]
+  references(): Reference[]
+  sentences(): Sentence[]
+  text(options?: object): string
+  wikitext(): string
+}
+
+declare class Image {
+  constructor(data: {
+    language?: string | undefined
+    lang?: string | null | undefined
+    domain?: string | undefined
+    file?: string | undefined
+    alt?: string | undefined
+    caption?: Sentence | undefined
+    wiki?: string | undefined
+  })
+  _language: string | null | undefined
+  _domain: string | undefined
+  _file: string
+  _alt: string | undefined
+  _caption: Sentence | undefined
+  _wiki: string | undefined
+
+  alt(): string
+  caption(): string
+  file(): string
+  format(): string | null
+  json(options?: object): object
+  links(): Link[]
+  src(): string
+  text(): string
+  thumb(size?: number): string
+  thumbnail(size?: number): string
+  url(): string
+  wikitext(): string
+}
+
+declare class Link {
+  constructor(data: object);
+  _text: any;
+  _type: any;
+  _raw: any;
+  _page: any;
+  
+  text(): string;
+  json(): object;
+  wikitext(): string;
+  page(str?: string): string;
+  anchor(str?: string): string;
+  _anchor: string | undefined;
+  wiki(str?: string): string | undefined;
+  _wiki: string | undefined;
+  type(str?: string): string;
+  site(str?: string): string;
+  _site: string | undefined;
+  href(): string;
+}
+
+declare class List {
+  constructor(data: Sentence[], wiki?: string)
+  _data: Sentence[]
+  _wiki: string
+
+  json(options?: object): object
+  lines(): object[]
+  links(clue: string): Link[]
+  text(): string
+  wikitext(): string
+}
+
+declare class Sentence {
+  constructor(data?: {
+    links?: Link[] | undefined
+    fmt?: object
+    wiki?: object
+    text?: object
+  })
+  _links: Link[]
+  _fmt: any
+  _wiki: any
+  _text: any
+
+  bold(clue?: number): string
+  bolds(): string[]
+  interwiki(): Link[]
+  isEmpty(): boolean
+  italic(clue?: number): string
+  italics(): string[]
+  json(options?: object): object
+  link(clue?: string): Link | undefined
+  links(clue?: string): Link[]
+  plaintext: (str?: string) => string
+  text(str?: string): string
+  wikitext(): string
+}
+
+export = wtf
+
+type version = string
+
+type fetchDefaults = {
+  path?: string | undefined;
+  wiki?: string | undefined;
+  domain?: string | undefined;
+  follow_redirects?: boolean | undefined;
+  lang?: string | undefined;
+  title?: string | number | Array<string> | Array<number> | undefined;
+  "Api-User-Agent"?: string | undefined;
+};
+
+type fetchCallback = (error: any, response: (null | Document | Document[])) => any;
+
+declare function fetch(title: string | number | Array<number> | Array<string>, options?: fetchDefaults | undefined, callback?: fetchCallback): Promise<null | Document | Document[]>;
+
+declare function wtf(wiki: string, options: object): Document
+declare namespace wtf {
+    export { fetch }
+    export { extend }
+    export { extend as plugin }
+    export { version }
+}
+
+declare function extend(fn: Function): {
+    (wiki: string, options: object): Document
+    fetch: (title: string | number | string[] | number[], options?: fetch.fetchDefaults | undefined, callback?: fetch.fetchCallback | undefined) => Promise<Document | Document[] | null>
+    extend: typeof extend
+    plugin: typeof extend
+    version: string
+}
