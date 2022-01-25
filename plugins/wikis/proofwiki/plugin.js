b6dbaad7ecffbb1fe4a5935795fa0f48f1fe88ba
spencer kelly <spencerkelly86@gmail.com>
2022-01-25 13:45:57 -0500

    cleanup wikis plugins

--- plugins/wikis/proofwiki/plugin.js
+++
@@ -1,7 +0,0 @@
-const templates = require('./templates')
-
-const plugin = (models, _templates) => {
-  // add new templates
-  Object.assign(_templates, templates)
-}
-module.exports = plugin
--- plugins/wikis/wikispecies/demo.js
+++
@@ -1,8 +0,0 @@
-const wtf = require('../../../src')
-wtf.extend(require('./plugin'))
-
-wtf.fetch('https://species.wikimedia.org/wiki/Balistapus_undulatus').then((doc) => {
-  console.log(doc.wikitext())
-  console.log(doc.templates().map((t) => t.json()))
-  console.log(doc.list().json())
-})
