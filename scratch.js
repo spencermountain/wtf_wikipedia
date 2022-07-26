0e97a95fa993ccde8b73892b8e27d2449f2903e0
spencer kelly <spencerkelly86@gmail.com>
2022-07-26 15:27:00 -0400

    fix for #488

--- scratch.js
+++ scratch.js
@@ -5,23 +5,28 @@ wtf.plugin(plg)
 // let coach = doc.infobox().get('coach')
 // coach.text() //'Nick Nurse'
 
-const str = `
-
+let str = `
 {{Escute
 | título     = "Loverboy"
 | arquivo    = Loverboy.ogg
 | descrição = {{pequeno|Uma amostra de "[[Loverboy (canção)|Loverboy]]", o primeiro ''single'' do seu oitavo álbum de estúdio e também trilha sonora do filme ''Glitter'' (2001), tornando-se assim no lançamento de estreia da artista com a distribuidora fonográfica Virgin Records.}}
 | posição    = esquerda
 }}
-
-
-
 `
-// const doc = await wtf.fetch('https://pt.m.wikipedia.org/wiki/Mariah_Carey')
-// console.log(doc.wikitext())
+// let doc = wtf(str)
+// console.log(doc.text())
+
+
+
+str = `[[File:Comprehensive Scale of Rape (2018)
+{{legend|#d7301f|Rape is endemic in this society}}
+foo]]`
 let doc = wtf(str)
-// console.log(doc.section('one').links())
 console.log(doc.text())
+
+
+
+// console.log(doc.section('one').links())
 // console.log(doc.templates('sustantivo masculino').map(t => t.json().template))
 // console.log(doc.section('sustantivo masculino'))
 // console.log(doc.json().sections)
