1d6a2800bb87ccbe6bf3cf454c19f4ac864730a0
spencer kelly <spencerkelly86@gmail.com>
2023-03-12 11:16:04 -0400

    fix start_date template for #519

--- scratch.js
+++ scratch.js
@@ -4,10 +4,8 @@ wtf.plugin(plg)
 
 let str = ` 
 {{Infobox officeholder
-  | predecessor2        = [[Dick Cheney]]
-  | successor2          = [[Mike Pence]]
-  | jr/sr3              = United States Senator
-  | term_end3           = January 15, 2009 
+|term_start   = {{start date|2022|May|6}}
+|term_end     = {{end date|2023|January|11}}
 }}`
 
 let doc = wtf(str)
