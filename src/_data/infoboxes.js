f7a1efe7e089026537c63132a1c4d2c4a4a4ec65
spencer kelly <spencermountain@gmail.com>
2023-11-23 09:13:26 -0500

    more infoboxes

--- src/_data/infoboxes.js
+++ src/_data/infoboxes.js
@@ -1,11 +1,13 @@
 export default [
   'infobox', //en
 
+  'amatl',
   'anfo', //mwl
   'anuāmapa', //haw
   'bilgi kutusu', //tr
   'bilgi', //tr
   'bilgiquti', //uz
+  'boaty fampahalalana',
   'boaty', //mg
   'boestkelaouiñ', //br
   'bosca', //ga
@@ -15,27 +17,42 @@ export default [
   'ficha', //es
   'generalni', //hr
   'gwybodlen3', //cy
+  'hộp thông tin',
   'info', //pt
+  'infoboesse 2',
   'infobokis', //tpi
   'infoboks', //da
+  'infobox deleted',
+  'infobox generic',
+  'infobox generiek',
   'infochascha', //rm
   'infokašćik', //dsb
   'infokast', //et
   'infokutija', //bs
   'infolentelė', //lt
+  'infookvir',
   'infopolje', //sl
   'informkesto', //eo
+  'infoschede',
   'infoskreine', //ltg
   'infotaula', //eu
   'inligtingskas',
   'inligtingskas3', //af
   'inligtingskas4', //af
-  'kishtey', //gv
+  'kishtey fys',
+  'kotak info',
   'kotak', //su
+  'məlumat qutusu',
+  'simple box',
+  'tertcita tanxe',
   'tertcita', //jbo
+  'tiätuloová',
   'tietolaatikko', //fi
+  'wd bosca sonraí',
   'yerleşim bilgi kutusu',
+  'ynfoboks generyk',
   'ynfoboks', //fy
+  'πλαίσιο πληροφοριών',
   'πλαίσιο', //el
   'акарточка', //ab
   'аҥа', //mhr
@@ -52,18 +69,19 @@ export default [
   'қуттӣ', //tg
   'ინფოდაფა', //ka
   'տեղեկաքարտ', //hy
-  'אינפאקעסטל', //yi
   'תבנית', //he
   'بطاقة', //ar
   'ڄاڻخانو', //sd
   'خانہ', //ur
   'لغة',
+  'معلوٗمات ڈَبہٕ',
   'ज्ञानसन्दूक', //hi
   'তথ্যছক', //bn
   'ਜਾਣਕਾਰੀਡੱਬਾ', //pa
   'సమాచారపెట్టె', //te
   'තොරතුරුකොටුව', //si
   'กล่องข้อมูล', //th
+  'ກ່ອງຂໍ້ມູນ',
   'ប្រអប់ព័ត៌មាន', //km
   '정보상자', //ko
   '明細', //zh_yue
