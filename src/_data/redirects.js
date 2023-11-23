bdd3abc358ed007e5b79c4c61ba7111dbe11212f
spencer kelly <spencermountain@gmail.com>
2023-11-23 08:47:56 -0500

    more redirect templates

--- src/_data/redirects.js
+++ src/_data/redirects.js
@@ -1,36 +1,71 @@
 export default [
   'aanstuur', //af
+  'aastiurey',
   'adkas', //br
+  'ailgyfeirio',
+  'alidirekto',
   'alih', //id
   'aýdaw',
+  'baw-ing',
   'beralîkirin', //ku
+  'birzuzendu',
+  'đổi hướng đến đây',
   'doorverwijzing', //nl
+  'header',
+  'i̇stiqamətləndirmə',
   'lencong', //ms
+  'ohjaa tänne',
   'ohjaus',
+  'omdirigering', //no
+  'pāradresācija',
   'patrz', //pl
+  'přesměrování',
   'přesměruj',
+  'preusmeritev',
+  'preusmjerava',
+  'preusmjerenje',
   'preusmjeri', //hr
+  'przekierowanie',
+  'redir',
+  'redirecció',
   'redireccion',
   'redirección', //es
   'redirecionamento', //pt
   'redirect', //en
+  'redirect3',
   'redirection', //fr
+  'redirige aquí',
+  'redirige',
+  'redirixe equí',
+  'rindirizz',
   'rinvia', //it
+  'stivre deike',
+  'suunamine',
   'tilvísun',
+  'trimite',
   'uudelleenohjaus',
-  'weiterleitung',
   'weiterleitung', //de
+  'weiterleitungshinweis',
+  'yoʻnaltirish',
   'yönlendi̇r',
-  'yönlendirme',
   'yönlendi̇rme', //tr
   'ανακατευθυνση', //el
   'айдау', //kk
+  'багыттама',
+  'буссинаби',
+  'дӏасахьажорг',
+  'от пренасочване',
+  'перанакіраванне',
   'перанакіраваньне',
+  'перанакіроўваецца сюды',
   'перенаправление', //ru
   'перенаправлення', //uk
+  'перенаправлено',
   'пренасочување', //mk
+  'преусмерава ',
   'преусмери', //sr
   'преусмјери',
+  'равонакунӣ',
   'ווייטערפירן', //yi
   'تحويل', //ar
   'تغییر_مسیر',
@@ -40,10 +75,13 @@ export default [
   'अनुप्रेषित', //hi
   'पुनर्निर्देशन', //hi
   'পুননির্দেশ', //bn
-  'เปลี่ยนทาง', //th
+  'পুনর্নির্দেশ',
+  'යළියොමුව',
+  'เปลี่ยนทาง',
   'ប្តូរទីតាំងទៅ', //km
+  '다른 뜻 넘어옴',
   'リダイレクト', //ja
+  '跳轉',
   '転送', //ja
   '重定向', //zh
-  'omdirigering', //no
 ]
